4c554ac5104c3d88f9092c5242465e464bd3d119
B0sto <bostoganashvilinika2019@gmail.com>
2026-04-21 18:33:54 +0400

    connection to db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,11 @@
 import express, { type Request, type Response } from 'express'
+import dotenv from 'dotenv';
+import { connectToDB } from './configs/db.js';
 
 const app = express();
+dotenv.config();
+connectToDB();
+
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Server is running fine")
